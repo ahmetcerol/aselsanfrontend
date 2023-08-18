fd758d5fa3b4df671e24f5bc6a21e770d66fb068
AhmetCanErol <erola2917@gmail.com>
2023-08-18 15:22:12 +0300

    18.08.23/4

--- src/components/AselsanKültür.js
+++ src/components/AselsanKültür.js
@@ -1,89 +1,83 @@
-import {styled} from "styled-components";
+import React from "react";
+import styled from "styled-components";
 import aselsanKültürKey from "../keys/AselsanKültürKey";
 
 const AselsanKültür = (props) => {
-
     return (
-    
-    <Container>
-        <Content>
-            <CTA>
-                <ParagrafBaşlık>{aselsanKültürKey.pageTitle}</ParagrafBaşlık>
-                <ParagrafIçerik>{aselsanKültürKey.pageInfo}</ParagrafIçerik>
-            </CTA>
-            <FixedIcon src="/images/aselsan-logo.png"/>
-        </Content>
-      <BgImage/>
-    </Container>);
-    
+        <Container>
+            <BgImage />
+            <Content>
+                <CTA>
+                    <PageTitle>{aselsanKültürKey.pageTitle}</PageTitle>
+                    <PageInfo>{aselsanKültürKey.pageInfo}</PageInfo>
+                </CTA>
+                <FixedIcon src="/images/aselsan-logo.png" alt="Aselsan Logo" />
+            </Content>
+        </Container>
+    );
 }
 
-
 const Container = styled.section`
- overflow: hidden;
- display: flex;
- flex-direction: column;
- text-align: center;
- height: 110vh;
- 
-`;
-const BgImage = styled.div `
-height: 100%;
-background-position: bottom;
-background-size: cover;
-background-repeat: no-repeat;
-background-image: url("/images/merhabalar.jpg");
-position: fixed;
-border-radius: 0 0 24px 24px;
-top: 0;
-right: 0;
-left: 0;
-z-index: -1;
-`;
-const Content = styled.div `
-margin-bottom: 10vw;
-position: relative;
-width:40%;
-min-height: 100vh;
-box-sizing: border-box;
+overflow: hidden;
 display: flex;
-justify-content: flex;
-align-items: flex-start;
 flex-direction: column;
-padding-top: 70px;
 `;
+
+const BgImage = styled.div`
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-image: url("/images/merhabalar.jpg");
+    background-size: cover;
+    background-position:bottom;
+    filter: brightness(0.6);
+    z-index: -1;
+`;
+
+const Content = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 80%;
+    margin: 0 auto;
+    z-index: 1;
+`;
+
 const CTA = styled.div`
- width: 100%;
- background-color: #f9f9f9;
- display: flex;
- flex-direction: column;
- border-radius: 0 0 34px 34px;
+    background-color: rgba(255, 255, 255, 0.9);
+    border-radius: 20px;
+    padding: 20px;
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+    width: 50%;
+    margin-top:100px;
+    position: relative;
+    overflow: hidden;
 `;
-const ParagrafBaşlık =styled.h3`
-color: #003087;
-text-align: center;
-font-size: 16px;
-font-weight: 700;
-margin-left: 20px;
-letter-spacing: 5px;
+
+const PageTitle = styled.h1`
+    color: #003087;
+    font-size: 32px;
+    font-weight: 700;
+    line-height: 1.2;
+    margin-bottom: 15px;
+    position: relative;
 `;
+
+const PageInfo = styled.p`
+    color: #333;
+    font-size: 18px;
+    line-height: 1.6;
+    text-indent: 20px;
+`;
+
 const FixedIcon = styled.img`
     position: fixed;
     bottom: 20px;
     right: 20px;
-    width: 100px;
-    height: auto;
+    width: 80px;
     z-index: 10;
-`;
-const ParagrafIçerik= styled.p`
-color: #003087;
-text-align: left;
-font-size: 13px;
-margin-left: 25px;
-margin-right: 25px;
-letter-spacing: 1px;
-line-height: 30px;
-text-indent:20px;
 `;
 
 export default AselsanKültür;
